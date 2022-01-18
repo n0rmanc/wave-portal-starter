2934707d07c352d1c0311486af8209676c929f49
n0rmanc <s965202030@gmail.com>
2022-01-18 22:17:17 +0800

    access user's account

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
-  const checkIfWalletIsConnected = () => {
-    /*
-     * First make sure we have access to window.ethereum
-     */
-    const { ethereum } = window;
+  const [currentAccount, setCurrentAccount] = useState('');
 
-    if (!ethereum) {
-      console.log('Make sure you have metamask!');
-    } else {
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have metamask!');
+        return;
+      }
       console.log('We have the ethereum object', ethereum);
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log('Found an authorized account:', account);
+        setCurrentAccount(account);
+      } else {
+        console.log('No authorized account found');
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
